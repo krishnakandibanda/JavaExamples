042ad85539b73956f4c0baaad6dbb2f48a41c73d
krishnakandibanda <39544828+krishnakandibanda@users.noreply.github.com>
2019-05-09 20:03:01 +0530

    Add Two Numbers and Test Class

---
+++ src/com/app/bean/AddTwoNumbers.java
@@ -0,0 +1,13 @@
+package com.app.bean;
+
+public class AddTwoNumbers {
+	
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 10;
+		System.out.println("Add Two Number  "+(a+b));
+		
+	}
+
+}
---
+++ src/com/app/test/Test.java
@@ -0,0 +1,16 @@
+package com.app.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is Test Class  ");
+	
+		for(int i=0;i<10;i++) {
+			System.out.println("jjjkkk "+i);
+		}
+		
+	}
+
+}
