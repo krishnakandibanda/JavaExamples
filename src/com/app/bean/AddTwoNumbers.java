b3e4151c220ec3215b472af6641f78ba76b69b4e
krishnakandibanda <39544828+krishnakandibanda@users.noreply.github.com>
2019-05-09 20:23:44 +0530

    Task 1546 Concat Two String & Demo Class

--- src/com/app/bean/AddTwoNumbers.java
+++ src/com/app/bean/AddTwoNumbers.java
@@ -4,9 +4,9 @@ public class AddTwoNumbers {
 	
 	public static void main(String[] args) {
 		
-		int a = 5;
-		int b = 10;
-		System.out.println("Add Two Number  "+(a+b));
+		String s="krishna";
+		String d="srini";
+		System.out.println("Concat two String  "+(s+d));
 		
 	}
 
--- src/com/app/test/Demo.java
+++ src/com/app/test/Demo.java
@@ -1,1 +1,15 @@
+package com.app.test;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is Test Class  ");
+
+			System.out.println("====== "+(51*10));
+			
+	}
+
+}
 
